c9249f77839cec3488982bbe72b6f42f4019f23f
SHINOYP <shinoyshinoo@gmail.com>
2024-10-22 22:26:30 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Navbar from "./Components/navbar";
 import Footer from "./Components/footer";
-import AnimatedRoutes from "../src/Components/AnimatedRoutes";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Project from "./pages/projects";
+import Contact from "./pages/contact";
 
 const React = require("react");
 
 export default function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <AnimatedRoutes />
-        <Footer />
-      </Router>
+      <Navbar />
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import App from './App';
-import './app.css'
-import { createRoot } from 'react-dom/client';
-const React=require('react');
+import "./app.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+const React = require("react");
 
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <Router>
+    <App tab="home" />
+  </Router>
+);
